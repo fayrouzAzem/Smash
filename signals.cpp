66361fd08f906e737ad6c83d64746f214ac87c76
Fayrouz Azem <104259576+fayrouzAzem@users.noreply.github.com>
2023-08-17 01:25:02 +0300

    Update signals.cpp

--- signals.cpp
+++ signals.cpp
@@ -1,102 +1,89 @@
-#include <iostream>
 #include <signal.h>
-#include "signals.h"
+
+#include <iostream>
+
 #include "Commands.h"
+#include "signals.h"
 
 using namespace std;
 
 void ctrlZHandler(int sig_num) {
-	// TODO: Add your implementation
-    cout<<"smash: got ctrl-Z"<<endl;
+	cout << "smash: got ctrl-Z" << endl;
 
+	int pid = SmallShell::getInstance().getCurrentJobPid();
 
-    int pid=SmallShell::getInstance().getCurrentJobPid();
+	const char* currentCommand = SmallShell::getInstance().getCurrentJobCommand().c_str();
+	if (pid == -1) {
+		return;
+	}
 
+	if (kill(pid, SIGSTOP) == -1) {
+		perror("smash error: kill failed");
+		return;
+	}
+	cout << "smash: process " << pid << " was stopped" << endl;
+	auto start = SmallShell::getInstance().jobs_list->jobs_entry_vec.begin();
+	auto end = SmallShell::getInstance().jobs_list->jobs_entry_vec.end();
+	auto length = SmallShell::getInstance().jobs_list->jobs_entry_vec.size();
+	for (auto p = start; p < end && (length != 0); p++) {
+		if (p->jobPid == pid) {
+			p->is_stopped = true;
+			p->insertion_time = time(NULL);
 
-    const char* currentCommand=SmallShell::getInstance().getCurrentJobCommand().c_str();
-    if(pid==-1)
-    {
-        return;
-    }
+			if (p->insertion_time == -1) {
+				perror("smash error: time failed");
+				return;
+			}
 
-
-    if(kill(pid,SIGSTOP) == -1)
-    {
-        perror("smash error: kill failed");
-        return;
-    }
-    cout<< "smash: process "<< pid <<" was stopped"<<endl;
-    auto start = SmallShell::getInstance().jobs_list->jobs_entry_vec.begin();
-    auto end = SmallShell::getInstance().jobs_list->jobs_entry_vec.end();
-    auto length = SmallShell::getInstance().jobs_list->jobs_entry_vec.size();
-    for (auto p = start;
-         p < end &&
-         (length != 0); p++) {
-        if (p->jobPid == pid) {
-            p->is_stopped = true;
-            p->insertion_time = time(NULL);
-
-            if (p->insertion_time == -1) {
-                perror("smash error: time failed");
-                return;
-            }
-
-            SmallShell::getInstance().setCurrJobId(-1);
-            SmallShell::getInstance().setCurrJobPid(-1);
-            SmallShell::getInstance().setCurrjobCommand("");
-            return;
-        }
-    }
-        Command * command=new ExternalCommand(currentCommand);
-    SmallShell::getInstance().jobs_list->addJob(command,pid,true);
-    SmallShell::getInstance().setCurrJobId(-1);
-    SmallShell::getInstance().setCurrJobPid(-1);
-    SmallShell::getInstance().setCurrjobCommand("");
-
-
+			SmallShell::getInstance().setCurrJobId(-1);
+			SmallShell::getInstance().setCurrJobPid(-1);
+			SmallShell::getInstance().setCurrjobCommand("");
+			return;
+		}
+	}
+	Command* command = new ExternalCommand(currentCommand);
+	SmallShell::getInstance().jobs_list->addJob(command, pid, true);
+	SmallShell::getInstance().setCurrJobId(-1);
+	SmallShell::getInstance().setCurrJobPid(-1);
+	SmallShell::getInstance().setCurrjobCommand("");
 }
 
 void ctrlCHandler(int sig_num) {
-    cout<<"smash: got ctrl-C"<<endl;
-    int pidId=SmallShell::getInstance().getCurrentJobPid();
+	cout << "smash: got ctrl-C" << endl;
+	int pidId = SmallShell::getInstance().getCurrentJobPid();
 
-    if(pidId>0)
-    {
-        if(kill(pidId,SIGKILL)==-1)
+	if (pidId > 0) {
+		if (kill(pidId, SIGKILL) == -1)
 
-        {
-            perror("smash error: kill failed");
-        }
-        else
-        {
-            cout<<"smash: process "<< pidId<<" was killed"<<endl;
-        }
-        auto start = SmallShell::getInstance().jobs_list->jobs_entry_vec.begin();
-        auto end = SmallShell::getInstance().jobs_list->jobs_entry_vec.end();
-        auto length = SmallShell::getInstance().jobs_list->jobs_entry_vec.size();
-        for (auto p = start;
-             p < end &&
-             (length != 0); p++) {
-        if(p->jobPid == pidId){
-            SmallShell::getInstance().jobs_list->jobs_entry_vec.erase(p);
-            auto last = SmallShell::getInstance().jobs_list->jobs_entry_vec.end();
+		{
+			perror("smash error: kill failed");
+		}
+		else {
+			cout << "smash: process " << pidId << " was killed" << endl;
+		}
+		auto start = SmallShell::getInstance().jobs_list->jobs_entry_vec.begin();
+		auto end = SmallShell::getInstance().jobs_list->jobs_entry_vec.end();
+		auto length = SmallShell::getInstance().jobs_list->jobs_entry_vec.size();
+		for (auto p = start; p < end && (length != 0); p++) {
+			if (p->jobPid == pidId) {
+				SmallShell::getInstance().jobs_list->jobs_entry_vec.erase(p);
+				auto last = SmallShell::getInstance().jobs_list->jobs_entry_vec.end();
 
-            if(SmallShell::getInstance().jobs_list->jobs_entry_vec.size() != 0)
-            {
-                last --;
-                SmallShell::getInstance().jobs_list->max_job_id = last->job_id;
-            }
-            else{
-                SmallShell::getInstance().jobs_list->max_job_id =0;
-            }
-        }
-        }
-        SmallShell::getInstance().setCurrJobId(-1);
-        SmallShell::getInstance().setCurrjobCommand("");
-        SmallShell::getInstance().setCurrJobPid(-1);
-    }
+				if (SmallShell::getInstance().jobs_list->jobs_entry_vec.size() != 0) {
+					last--;
+					SmallShell::getInstance().jobs_list->max_job_id = last->job_id;
+				}
+				else {
+					SmallShell::getInstance().jobs_list->max_job_id = 0;
+				}
+			}
+		}
+		SmallShell::getInstance().setCurrJobId(-1);
+		SmallShell::getInstance().setCurrjobCommand("");
+		SmallShell::getInstance().setCurrJobPid(-1);
+	}
 }
 
 void alarmHandler(int sig_num) {
-  // TODO: Add your implementation
+	// TODO: Add your implementation
 }
